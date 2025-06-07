594740afcb929c57b34747c95d3ff85437e7f317
Mixail <ssmitss58@gmail.com>
2025-06-07 12:31:53 +0300

    feat: added all possible settings

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+export default {
+    extends: ['stylelint-config-standard'],
+    rules: {
+        'color-function-notation': 'legacy',
+        'color-function-alias-notation': null,
+        'alpha-value-notation': 'number',
+        'value-keyword-case': null,
+        'color-hex-length': 'long',
+    },
+};
