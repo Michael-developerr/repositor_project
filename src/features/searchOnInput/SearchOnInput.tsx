e1c13fc187118e2a2314285c6a77d9407a48e327
Mixail <ssmitss58@gmail.com>
2025-06-10 01:50:30 +0300

    feat: add featcher input

--- src/features/searchOnInput/SearchOnInput.tsx
+++ src/features/searchOnInput/SearchOnInput.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../shared/ui/input/Input';
+import { useNavigate } from 'react-router-dom';
 
 const SearchOnInput = () => {
-    return <Input placeholder="Enter GitHub username" />;
+    const [value, setValue] = useState('');
+    const navigate = useNavigate();
+
+    const handleKeyDown = (
+        e: React.KeyboardEvent<HTMLInputElement>,
+    ) => {
+        if (e.key === 'Enter' && value.trim()) {
+            navigate(`/user/${value.trim()}`);
+        }
+    };
+
+    return (
+        <Input
+            placeholder="Enter GitHub username"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+        />
+    );
 };
 
 export default SearchOnInput;
