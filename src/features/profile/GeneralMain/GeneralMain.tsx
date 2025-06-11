58d0aa199a8f63ea8a7f87ae77f2793efcd978e2
Mixail <ssmitss58@gmail.com>
2025-06-11 21:29:38 +0300

    feat: all work

--- src/features/profile/GeneralMain/GeneralMain.tsx
+++ src/features/profile/GeneralMain/GeneralMain.tsx
@@ -2,13 +2,21 @@ import ProfileUser from '../../../entities/user-profile/ProfileUser';
 import ProfileRepositoria from '../../../entities/user-repositoria/ProfileRepositoria';
 import useProfileData from '../api/useProfileData';
 import style from './GeneralMain.module.css';
-
+import searchUserlogo from '../../../shared/icons/main/searchUser.svg';
 const GeneralMain = () => {
     const { user } = useProfileData();
     const { userData, isUserLoading, userError } = user;
 
-    if (userError) {
-        <p>Start with searching a GitHub user</p>;
+    if (!userData) {
+        return (
+            <div className={style.nowSearch}>
+                <img
+                    src={searchUserlogo}
+                    alt="Search user"
+                />
+                <p>Start with searching a GitHub user</p>
+            </div>
+        );
     }
     return (
         <ul className={style.listGeneral}>
--- src/shared/api/repositories/endpoints.ts
+++ src/shared/api/repositories/endpoints.ts
@@ -12,17 +12,6 @@ export const endpoints = createApi({
     reducerPath: 'githubApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://api.github.com',
-        prepareHeaders: (headers) => {
-            const token = import.meta.env.VITE_GITHUB_TOKEN;
-
-            if (token) {
-                headers.set(
-                    'Authorization',
-                    `Bearer ${token}`,
-                );
-            }
-            return headers;
-        },
     }),
     endpoints: (builder) => ({
         getCurrentUser: builder.query<GitHubUser, void>({
